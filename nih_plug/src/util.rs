7fe47dfbbddc4e24fa8c24c987ad77f640c71305
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-01-26 11:48:40 +0100

    Add a decibel to gain conversion function

--- nih_plug/src/util.rs
+++ nih_plug/src/util.rs
@@ -14,7 +14,33 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub mod debug;
-pub mod params;
-pub mod plugin;
-pub mod util;
+pub const MINUS_INFINITY_DB: f32 = -100.0;
+
+/// Convert decibels to gain, treating anything below -100 dB as minus infinity.
+pub fn db_to_gain(dbs: f32) -> f32 {
+    if dbs > MINUS_INFINITY_DB {
+        10.0f32.powf(dbs * 0.05)
+    } else {
+        0.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_db_to_gain_positive() {
+        assert_eq!(db_to_gain(3.0), 1.4125376);
+    }
+
+    #[test]
+    fn test_db_to_gain_negative() {
+        assert_eq!(db_to_gain(-3.0), 1.4125376f32.recip());
+    }
+
+    #[test]
+    fn test_db_to_gain_minus_infinity() {
+        assert_eq!(db_to_gain(-100.0), 0.0);
+    }
+}
