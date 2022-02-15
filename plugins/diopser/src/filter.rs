935d952d8135d294caead67f3879d318ef23f760
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-02-15 18:00:01 +0100

    Make the biquads SIMD capable

--- plugins/diopser/src/filter.rs
+++ plugins/diopser/src/filter.rs
@@ -14,43 +14,56 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use packed_simd::f32x2;
 use std::f32::consts;
+use std::ops::{Add, Mul, Sub};
 
 /// A simple biquad filter with functions for generating coefficients for an all-pass filter.
 ///
 /// Based on <https://en.wikipedia.org/wiki/Digital_biquad_filter#Transposed_direct_forms>.
+///
+/// The type parameter T  should be either an `f32` or a SIMD type.
 #[derive(Clone, Copy, Debug)]
-pub struct Biquad {
-    pub coefficients: BiquadCoefficients,
-    s1: f32,
-    s2: f32,
+pub struct Biquad<T> {
+    pub coefficients: BiquadCoefficients<T>,
+    s1: T,
+    s2: T,
 }
 
 /// The coefficients `[b0, b1, b2, a1, a2]` for [Biquad]. These coefficients are all prenormalized,
 /// i.e. they have been divided by `a0`.
+///
+/// The type parameter T  should be either an `f32` or a SIMD type.
 #[derive(Clone, Copy, Debug)]
-pub struct BiquadCoefficients {
-    b0: f32,
-    b1: f32,
-    b2: f32,
-    a1: f32,
-    a2: f32,
+pub struct BiquadCoefficients<T> {
+    b0: T,
+    b1: T,
+    b2: T,
+    a1: T,
+    a2: T,
 }
 
-impl Default for Biquad {
+/// Either an `f32` or some SIMD vector type of `f32`s that can be used iwth our biquads.
+pub trait SimdType:
+    Mul<Output = Self> + Sub<Output = Self> + Add<Output = Self> + Copy + Sized
+{
+    fn from_f32(value: f32) -> Self;
+}
+
+impl<T: SimdType> Default for Biquad<T> {
     /// Before setting constants the filter should just act as an identity function.
     fn default() -> Self {
         Self {
             coefficients: BiquadCoefficients::identity(),
-            s1: 0.0,
-            s2: 0.0,
+            s1: T::from_f32(0.0),
+            s2: T::from_f32(0.0),
         }
     }
 }
 
-impl Biquad {
+impl<T: SimdType> Biquad<T> {
     /// Process a single sample.
-    pub fn process(&mut self, sample: f32) -> f32 {
+    pub fn process(&mut self, sample: T) -> T {
         let result = self.coefficients.b0 * sample + self.s1;
 
         self.s1 = self.coefficients.b1 * sample - self.coefficients.a1 * result + self.s2;
@@ -60,16 +73,27 @@ impl Biquad {
     }
 }
 
-impl BiquadCoefficients {
+impl<T: SimdType> BiquadCoefficients<T> {
+    /// Convert scalar coefficients into the correct vector type.
+    pub fn from_f32s(scalar: BiquadCoefficients<f32>) -> Self {
+        Self {
+            b0: T::from_f32(scalar.b0),
+            b1: T::from_f32(scalar.b1),
+            b2: T::from_f32(scalar.b2),
+            a1: T::from_f32(scalar.a1),
+            a2: T::from_f32(scalar.a2),
+        }
+    }
+
     /// Filter coefficients that would cause the sound to be passed through as is.
     pub fn identity() -> Self {
-        Self {
+        Self::from_f32s(BiquadCoefficients {
             b0: 1.0,
             b1: 0.0,
             b2: 0.0,
             a1: 0.0,
             a2: 0.0,
-        }
+        })
     }
 
     /// Compute the coefficients for an all-pass filter.
@@ -93,6 +117,18 @@ impl BiquadCoefficients {
         let a1 = (-2.0 * cos_omega0) / a0;
         let a2 = (1.0 - alpha) / a0;
 
-        Self { b0, b1, b2, a1, a2 }
+        Self::from_f32s(BiquadCoefficients { b0, b1, b2, a1, a2 })
+    }
+}
+
+impl SimdType for f32 {
+    fn from_f32(value: f32) -> Self {
+        value
+    }
+}
+
+impl SimdType for f32x2 {
+    fn from_f32(value: f32) -> Self {
+        f32x2::splat(value)
     }
 }
