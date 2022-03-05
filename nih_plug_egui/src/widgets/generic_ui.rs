05d97bb71e362ad3a9bf87f8ff96355bcc7a2bf2
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-03-05 19:16:20 +0100

    Add a super simple egui generic UI widget

---
+++ nih_plug_egui/src/widgets/generic_ui.rs
@@ -0,0 +1,55 @@
+//! A simple generic UI widget that renders all parameters in a [`Params`] object as a scrollable
+//! list of sliders and labels.
+
+use std::pin::Pin;
+
+use egui::Ui;
+use nih_plug::context::ParamSetter;
+use nih_plug::param::internals::ParamPtr;
+use nih_plug::prelude::{Param, Params};
+
+use super::ParamSlider;
+
+/// A widget that can be used to create a generic UI with. This is used in conjuction with empty
+/// structs to emulate existential types.
+pub trait ParamWidget {
+    fn add_widget<P: Param>(&self, ui: &mut Ui, param: &P, setter: &ParamSetter);
+
+    unsafe fn add_widget_raw(&self, ui: &mut Ui, param: &ParamPtr, setter: &ParamSetter) {
+        match param {
+            ParamPtr::FloatParam(p) => self.add_widget(ui, &**p, setter),
+            ParamPtr::IntParam(p) => self.add_widget(ui, &**p, setter),
+            ParamPtr::BoolParam(p) => self.add_widget(ui, &**p, setter),
+            ParamPtr::EnumParam(p) => self.add_widget(ui, &**p, setter),
+        }
+    }
+}
+
+/// Create a generic UI using [`ParamSlider`]s.
+pub struct GenericSlider;
+
+/// Create a scrollable generic UI using the specified widget. Takes up all the remaining vertical
+/// space.
+pub fn create(
+    ui: &mut Ui,
+    params: Pin<&dyn Params>,
+    setter: &ParamSetter,
+    widget: impl ParamWidget,
+) {
+    let param_map = params.param_map();
+    let param_ids = params.param_ids();
+
+    egui::containers::ScrollArea::vertical().show(ui, |ui| {
+        for id in param_ids {
+            let param = param_map[id];
+            ui.label(unsafe { param.name() });
+            unsafe { widget.add_widget_raw(ui, &param, setter) };
+        }
+    });
+}
+
+impl ParamWidget for GenericSlider {
+    fn add_widget<P: Param>(&self, ui: &mut Ui, param: &P, setter: &ParamSetter) {
+        ui.add(ParamSlider::for_param(param, setter));
+    }
+}
