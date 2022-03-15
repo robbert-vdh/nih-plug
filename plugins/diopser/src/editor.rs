01e7998c037c8e292d3b3b491d7538374ba3da96
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-03-15 13:53:31 +0100

    Revert Diopser back to egui
    I should probably first redo the Gain GUI example with vizia before
    deciding on a final GUI framework for Diopser.

--- plugins/diopser/src/editor.rs
+++ plugins/diopser/src/editor.rs
@@ -14,99 +14,26 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use nih_plug::prelude::{Editor, GuiContext, Param};
-use nih_plug_iced::widgets::ParamMessage;
-use nih_plug_iced::{create_iced_editor, Command, Element, IcedEditor, IcedState};
+use nih_plug::prelude::Editor;
+use nih_plug_egui::widgets::generic_ui;
+use nih_plug_egui::{create_egui_editor, egui, EguiState};
 use std::pin::Pin;
 use std::sync::Arc;
 
 use crate::DiopserParams;
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
-pub fn default_state() -> Arc<IcedState> {
-    IcedState::from_size(600, 400)
+pub fn default_state() -> Arc<EguiState> {
+    EguiState::from_size(220, 330)
 }
 
 pub fn create(
     params: Pin<Arc<DiopserParams>>,
-    editor_state: Arc<IcedState>,
+    editor_state: Arc<EguiState>,
 ) -> Option<Box<dyn Editor>> {
-    create_iced_editor::<DiopserEditor>(editor_state, params)
-}
-
-struct DiopserEditor {
-    params: Pin<Arc<DiopserParams>>,
-    context: Arc<dyn GuiContext>,
-
-    // FIXME: All of this is just to test the reactivity
-    button_state: nih_plug_iced::widget::button::State,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    /// Update a parameter's value.
-    ParamUpdate(ParamMessage),
-}
-
-impl IcedEditor for DiopserEditor {
-    type Executor = nih_plug_iced::executor::Default;
-    type Message = Message;
-    type InitializationFlags = Pin<Arc<DiopserParams>>;
-
-    fn new(
-        params: Self::InitializationFlags,
-        context: Arc<dyn GuiContext>,
-    ) -> (Self, Command<Self::Message>) {
-        let editor = DiopserEditor {
-            params,
-            context,
-            button_state: Default::default(),
-        };
-
-        (editor, Command::none())
-    }
-
-    fn context(&self) -> &dyn GuiContext {
-        self.context.as_ref()
-    }
-
-    fn update(
-        &mut self,
-        _window: &mut nih_plug_iced::WindowQueue,
-        message: Self::Message,
-    ) -> Command<Self::Message> {
-        match message {
-            Message::ParamUpdate(message) => self.handle_param_message(message),
-        }
-
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<'_, Self::Message> {
-        nih_plug_iced::Row::new()
-            .height(nih_plug_iced::Length::Fill)
-            .align_items(nih_plug_iced::Alignment::Center)
-            .push(
-                nih_plug_iced::Column::new()
-                    .width(nih_plug_iced::Length::Fill)
-                    .align_items(nih_plug_iced::Alignment::Center)
-                    .push(nih_plug_iced::Text::new(format!(
-                        "{} filters active",
-                        self.params.filter_stages.value
-                    )))
-                    .push(
-                        nih_plug_iced::Button::new(
-                            &mut self.button_state,
-                            nih_plug_iced::Text::new("MAXIMUM POWAH"),
-                        )
-                        .on_press(Message::ParamUpdate(
-                            ParamMessage::SetParameterNormalized(
-                                self.params.filter_stages.as_ptr(),
-                                1.0,
-                            ),
-                        )),
-                    ),
-            )
-            .into()
-    }
+    create_egui_editor(editor_state, (), move |egui_ctx, setter, _state| {
+        egui::CentralPanel::default().show(egui_ctx, |ui| {
+            generic_ui::create(ui, params.as_ref(), setter, generic_ui::GenericSlider);
+        });
+    })
 }
