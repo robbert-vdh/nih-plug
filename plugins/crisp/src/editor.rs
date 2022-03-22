31a84d51ddcfe3ff0863f35c831dbecbff6c4465
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-03-22 19:50:40 +0100

    Make the Crisp editor slightly taller

--- plugins/crisp/src/editor.rs
+++ plugins/crisp/src/editor.rs
@@ -36,7 +36,7 @@ impl Model for Data {}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::from_size(400, 330)
+    ViziaState::from_size(400, 390)
 }
 
 pub(crate) fn create(
