7cc05fce9a555d3bfd8ac9b3fe4ea011d8c89336
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-04-07 14:13:03 +0200

    Use crate instead of super for prelude reexports

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,25 +1,25 @@
 // Re-export the macros, derive macros are already re-exported ferom their respectivem odules
-pub use super::nih_debug_assert;
-pub use super::nih_debug_assert_eq;
-pub use super::nih_debug_assert_failure;
-pub use super::nih_debug_assert_ne;
-pub use super::nih_export_clap;
-pub use super::nih_export_vst3;
-pub use super::nih_log;
+pub use crate::nih_debug_assert;
+pub use crate::nih_debug_assert_eq;
+pub use crate::nih_debug_assert_failure;
+pub use crate::nih_debug_assert_ne;
+pub use crate::nih_export_clap;
+pub use crate::nih_export_vst3;
+pub use crate::nih_log;
 
-pub use super::formatters;
-pub use super::util;
+pub use crate::formatters;
+pub use crate::util;
 
-pub use super::buffer::Buffer;
-pub use super::context::{GuiContext, ParamSetter, ProcessContext};
+pub use crate::buffer::Buffer;
+pub use crate::context::{GuiContext, ParamSetter, ProcessContext};
 // This also includes the derive macro
-pub use super::param::enums::{Enum, EnumParam};
-pub use super::param::internals::{ParamPtr, Params};
-pub use super::param::range::{FloatRange, IntRange};
-pub use super::param::smoothing::{Smoother, SmoothingStyle};
-pub use super::param::{BoolParam, FloatParam, IntParam, Param, ParamFlags};
-pub use super::plugin::{
+pub use crate::param::enums::{Enum, EnumParam};
+pub use crate::param::internals::{ParamPtr, Params};
+pub use crate::param::range::{FloatRange, IntRange};
+pub use crate::param::smoothing::{Smoother, SmoothingStyle};
+pub use crate::param::{BoolParam, FloatParam, IntParam, Param, ParamFlags};
+pub use crate::plugin::{
     BufferConfig, BusConfig, ClapPlugin, Editor, NoteEvent, ParentWindowHandle, Plugin,
     ProcessStatus, Vst3Plugin,
 };
-pub use super::wrapper::state::State;
+pub use crate::wrapper::state::State;
