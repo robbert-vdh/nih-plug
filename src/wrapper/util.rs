3b0291b4f55be4fa7ff74b808abde98c08b9cae3
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-04-24 15:03:27 +0200

    Add function for setting up file and windbg logs
    And also writing panics to those logs.

--- src/wrapper/util.rs
+++ src/wrapper/util.rs
@@ -1,6 +1,11 @@
 use std::cmp;
+use std::fs::File;
 use std::marker::PhantomData;
 use std::os::raw::c_char;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+/// The environment variable for controlling the logging behavior.
+const NIH_LOG_ENV: &str = "NIH_LOG";
 
 #[cfg(all(debug_assertions, feature = "assert_process_allocs"))]
 #[global_allocator]
@@ -37,6 +42,83 @@ pub fn strlcpy(dest: &mut [c_char], src: &str) {
     let copy_len = cmp::min(dest.len() - 1, src.len());
     dest[..copy_len].copy_from_slice(&src_bytes_signed[..copy_len]);
     dest[copy_len] = 0;
+}
+
+/// Set up the logger so that the `nih_*!()` logging and assertion macros log output to a
+/// centralized location and panics also get written there. By default this logs to STDERR unless
+/// the user is running Windows and a debugger has been attached, in which case
+/// `OutputDebugString()` will be used instead.
+///
+/// The logger's behavior can be controlled by setting the `NIH_LOG` environment variable to:
+///
+/// - `stderr`, in which case the log output always gets written to STDERR.
+/// - `windbg` (only on Windows), in which case the output always gets logged using
+///   `OutputDebugString()`.
+/// - A file path, in which case the output gets appended to the end of that file which will be
+///   created if necessary.
+///
+/// # TODO
+///
+/// Document `NIH_LOG` in an easier to find place.
+pub fn setup_logger() {
+    // `win_dbg_logger` has no way to let us know that the logger has already been set up, so we'll
+    // need to do it this way
+    static LOGGER_SET_UP: AtomicBool = AtomicBool::new(false);
+    if LOGGER_SET_UP
+        .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+        .is_err()
+    {
+        return;
+    }
+
+    let nih_log_env = std::env::var(NIH_LOG_ENV);
+    let nih_log_env_str = nih_log_env.as_deref().unwrap_or("");
+
+    #[cfg(target_os = "windows")]
+    if nih_log_env_str.eq_ignore_ascii_case("windbg") || win_dbg_logger::is_debugger_present() {
+        win_dbg_logger::init();
+        log_panics::init();
+        return;
+    }
+
+    // If opening the file fails, then we'll log to STDERR anyways, hence this closure
+    let logger_config = simplelog::ConfigBuilder::new()
+        .set_thread_mode(simplelog::ThreadLogMode::Both)
+        .build();
+    let init_stderr_logger = || {
+        simplelog::TermLogger::init(
+            simplelog::LevelFilter::Trace,
+            logger_config.clone(),
+            simplelog::TerminalMode::Stderr,
+            simplelog::ColorChoice::Auto,
+        )
+    };
+
+    // If the logger has already been set up outside of this function then that won't cause any
+    // problems, so we can ignore the results here
+    if nih_log_env_str.eq_ignore_ascii_case("stderr") || nih_log_env_str.is_empty() {
+        let _ = init_stderr_logger();
+    } else {
+        let file = File::options()
+            .append(true)
+            .create(true)
+            .open(nih_log_env_str);
+        match file {
+            Ok(file) => {
+                let _ = simplelog::WriteLogger::init(
+                    simplelog::LevelFilter::Trace,
+                    logger_config,
+                    file,
+                );
+            }
+            Err(err) => {
+                let _ = init_stderr_logger();
+                nih_debug_assert_failure!("Could not open '{}': {:?}", nih_log_env_str, err);
+            }
+        }
+    }
+
+    log_panics::init();
 }
 
 /// A wrapper around the entire process function, including the plugin wrapper parts. This sets up
